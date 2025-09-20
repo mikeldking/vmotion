5717085a0edc4d5753c842ebee986edf8a65d9b2
Mikyo King <mikyo@arize.com>
2025-09-20 11:11:42 -0600

    use classes

--- index.js
+++ index.js
@@ -5,21 +5,41 @@ let x = 0;
 let y = 0;
 const width = 20;
 const height = 20;
-let xSpeed = 3;
-let ySpeed = 3;
+
+class Square {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.size = 20;
+    }
+    moveRight() {
+        this.x += this.size;
+    }
+    moveLeft() {
+        this.x -= this.size;
+    }
+    moveDown() {
+        this.y += this.size;
+    }
+    moveUp() {
+        this.y -= this.size;
+    }
+}
+
+const square = new Square();
 
 document.addEventListener("keydown", (event) => {
     if (event.key === "j") {
-        y += height;
+        square.moveDown();
     }
     if (event.key === "k") {
-        y -= height;
+        square.moveUp();
     }
     if (event.key === "h") {
-        x -= width;
+        square.moveLeft();
     }
     if (event.key === "l") {
-        x += width;
+        square.moveRight();
     }
 });
 function clear() {
@@ -27,23 +47,10 @@ function clear() {
 }
 function draw() {
     ctx.fillStyle = "green";
-    ctx.fillRect(x, y, width, height);
+    ctx.fillRect(square.x, square.y, square.size, square.size);
     console.log("rendered");
 }
-
 function loop() {
-    // x += xSpeed;
-    // y += ySpeed;
-    // const left = x;
-    // const top = y;
-    // const right = x + width;
-    // const bottom = y + height;
-    // if (right > canvas.width || left < 0) {
-    //     xSpeed = -xSpeed;
-    // }
-    // if (bottom > canvas.height || top < 0) {
-    //     ySpeed = -ySpeed;
-    // }
     clear();
     draw();
     requestAnimationFrame(loop);
